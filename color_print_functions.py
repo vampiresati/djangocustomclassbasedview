2462b338fb7023903f17a84723dcfa4751a3fad7
satvir_sati <sssatvir12@gmail.com>
2023-04-08 20:19:08 +0530

    color print functions

---
+++ color_print_functions.py
@@ -0,0 +1,41 @@
+def red(words):
+    return "\033[31m\033[49m%s\033[0m" % words
+def white(words):
+    return "\033[37m\033[49m%s\033[0m" % words
+def blue(words):
+    return "\033[34m\033[49m%s\033[0m" % words
+def teal(words):
+    return "\033[36m\033[49m%s\033[0m" % words
+
+def black(words):
+    return "\033[30m%s\033[0m" % words
+def red(words):
+    return "\033[31m%s\033[0m" % words
+def green(words):
+    return "\033[32m%s\033[0m" % words
+def yellow(words):
+    return "\033[33m%s\033[0m" % words
+def blue(words):
+    return "\033[34m%s\033[0m" % words
+def magenta(words):
+    return "\033[35m%s\033[0m" % words
+def cyan(words):
+    return "\033[36m%s\033[0m" % words
+def white(words):
+    return "\033[37m%s\033[0m" % words
+def bright_black(words):
+    return "\033[30;1m%s\033[0m" % words
+def bright_red(words):
+    return "\033[31;1m%s\033[0m" % words
+def bright_green(words):
+    return "\033[32;1m%s\033[0m" % words
+def bright_yellow(words):
+    return "\033[33;1m%s\033[0m" % words
+def bright_blue(words):
+    return "\033[34;1m%s\033[0m" % words
+def bright_magenta(words):
+    return "\033[35;1m%s\033[0m" % words
+def bright_cyan(words):
+    return "\033[36;1m%s\033[0m" % words
+def bright_white(words):
+    return "\033[37;1m%s\033[0m" % words
